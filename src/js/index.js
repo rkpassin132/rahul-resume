d07f33e26a4799e6b9e6a03455fb8e67b41af49d
Rahul kumar <rkpassin132@gmail.com>
2023-04-17 19:16:59 +0530

    add work section

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 const AppController = (function () {
   // do not mess global space
-  'use strict';
+  "use strict";
 
   // helpers
   const select = (DOMElement) => document.querySelector(DOMElement);
@@ -8,13 +8,13 @@ const AppController = (function () {
 
   // object to hold references to html selectors
   const DOMElements = {
-    grid: '.grid',
-    gridItem: '.grid__item',
-    filterItem: '.filter__item',
-    accordion: '.accordion',
-    acActive: '.accordion.active',
-    animIcon: '.anim-icon',
-    vsSection: '#vs-section',
+    grid: ".grid",
+    gridItem: ".grid__item",
+    filterItem: ".filter__item",
+    accordion: ".accordion",
+    acActive: ".accordion.active",
+    animIcon: ".anim-icon",
+    vsSection: "#vs-section",
   };
 
   // app state
@@ -24,12 +24,76 @@ const AppController = (function () {
     iso: {},
   };
 
+  // private method
+  function initIsotopeData() {
+    let potfolioHtml = "";
+    potfolioData.forEach((p) => {
+      potfolioHtml += `<div class="grid__item ${p.filter_class}">
+            <figure class="grid__item__thumbnail">
+              <img src="${p.image}" alt="smoothies"/>
+            </figure>
+            <div class="grid__item__details flex flex-jc-sb flex-ai-c">
+              <div class="flex-wrap">
+                <h6 class="app-name">${p.name}</h6>
+                <p class="caption app-type">${p.caption}</p>
+              </div>`;
+      if (
+        p.live_link != undefined &&
+        p.live_link != null &&
+        p.live_link.length > 0
+      ) {
+        potfolioHtml += `<a href="${p.live_link}" target="_blank" class="redirect live_redirect" title="Preview"></a>`;
+      }
+      if (
+        p.source_link != undefined &&
+        p.source_link != null &&
+        p.source_link.length > 0
+      ) {
+        potfolioHtml += `<a href="${p.source_link}" target="_blank" class="redirect source_redirect" title="Source Redirect"></a>`;
+      }
+      potfolioHtml += `</div></div>`;
+    });
+    select(DOMElements.grid).innerHTML = potfolioHtml;
+  }
+
+  // private methods
+  // @desc initialize Isotope
+  function initIsotope() {
+    const collection = select(DOMElements.grid),
+      options = {
+        itemSelector: DOMElements.gridItem,
+        masonry: {
+          columnWidth: 1,
+          rowWidth: 1,
+          isFitWidth: true,
+        },
+      };
+    state.filterItems = selectAll(DOMElements.filterItem);
+
+    // instantiate Isotope
+    state.iso = new Isotope(collection, options);
+
+    // hookup click event for each "filter-item"
+    state.filterItems.forEach((item) =>
+      item.addEventListener("click", filterIso)
+    );
+  }
+
+  // @desc filter isotope elements
+  function filterIso(evt) {
+    changeActiveClass(evt.target);
+    // get filter value of clicked item
+    let filterValue = evt.target.dataset.filter;
+
+    return state.iso.arrange({ filter: filterValue }), !1;
+  }
+
   function changeActiveClass(element) {
     // remove "active" class from all filter items
-    state.filterItems.forEach((item) => item.classList.remove('active'));
+    state.filterItems.forEach((item) => item.classList.remove("active"));
 
     // adds "active" class on clicked item
-    element.classList.add('active');
+    element.classList.add("active");
   }
 
   // @desc initialize accordion
@@ -40,23 +104,23 @@ const AppController = (function () {
     if (acActive) {
       // set default height of active accordion
       let acPanel = acActive.nextElementSibling;
-      acPanel.style.maxHeight = acPanel.scrollHeight + 'px';
+      acPanel.style.maxHeight = acPanel.scrollHeight + "px";
     }
 
     // hookup click event for each "accordion"
     accordions.forEach((acc) =>
-      acc.addEventListener('click', onAccordionClicked)
+      acc.addEventListener("click", onAccordionClicked)
     );
   }
 
   function onAccordionClicked(evt) {
     // toggle "active" class on clicked item
-    evt.target.classList.toggle('active');
+    evt.target.classList.toggle("active");
 
     // Get and set panel max-height
     let panel = evt.target.nextElementSibling;
     panel.style.maxHeight = !panel.style.maxHeight
-      ? panel.scrollHeight + 'px'
+      ? panel.scrollHeight + "px"
       : null;
   }
 
@@ -91,8 +155,8 @@ const AppController = (function () {
       state.controls[index].setSpeed(0.6);
 
       // hookup mouse events
-      icon.addEventListener('mouseover', startAnimation(index));
-      icon.addEventListener('mouseleave', stopAnimation(index));
+      icon.addEventListener("mouseover", startAnimation(index));
+      icon.addEventListener("mouseleave", stopAnimation(index));
     });
   }
   function startAnimation(index) {
@@ -110,6 +174,8 @@ const AppController = (function () {
     init() {
       // invoke methods
       // initSmoothScroll();
+      initIsotopeData();
+      initIsotope();
       initAccordion();
       initLottieAnimation();
     },
@@ -118,7 +184,6 @@ const AppController = (function () {
 
 // will need to call a method to run the app on page load
 AppController.init();
-function scrollTo(section_class){
-  document.getElementById(section_class).scrollIntoView({behavior: 'smooth'});
+function scrollTo(section_class) {
+  document.getElementById(section_class).scrollIntoView({ behavior: "smooth" });
 }
-
---
+++ src/json-data/portfolio.js
@@ -0,0 +1,42 @@
+const potfolioData = [
+  {
+    name: "Book Reader",
+    caption: "React Native",
+    image: "src/assets/images/portfolio/book-reader.gif",
+    source_link: "https://github.com/rkpassin132/book-reader-app",
+    live_link: "",
+    filter_class: "app",
+  },
+  {
+    name: "College Attendance Management",
+    caption: "PHP | MySql",
+    image: "src/assets/images/portfolio/attendance-management.gif",
+    source_link: "https://github.com/rkpassin132/college-attendance",
+    live_link: "",
+    filter_class: "website",
+  },
+  {
+    name: "SEO plugin",
+    caption: "Javascript",
+    image: "src/assets/images/portfolio/seo-plugin.jpg",
+    source_link: "https://github.com/rkpassin132/seo-plugin",
+    live_link: "",
+    filter_class: "plugin",
+  },
+  {
+    name: "Destination map",
+    caption: "React | Google map API",
+    image: "src/assets/images/portfolio/desitnation.png",
+    source_link: "https://github.com/rkpassin132/destination-map",
+    live_link: "https://rkpassin132-destination-map.netlify.app",
+    filter_class: "website",
+  },
+  {
+    name: "Quick WA Chat",
+    caption: "Android | Java",
+    image: "src/assets/images/portfolio/wa.gif",
+    source_link: "",
+    live_link: "https://play.google.com/store/apps/details?id=com.nukepin.quickwachat",
+    filter_class: "app",
+  },
+];
